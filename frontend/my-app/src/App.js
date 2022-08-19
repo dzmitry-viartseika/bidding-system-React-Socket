d5fa00143e52bec6f9dbf490ead8de55ac135a08
Dzmitry Viartseika <dviartseika@exadel.com>
2022-08-19 14:28:16 +0200

    added frontend

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./components/Home"
+import AddProduct from "./components/AddProduct"
+import BidProduct from "./components/BidProduct"
+import Products from "./components/Products"
+import {Route, Routes} from "react-router-dom"
+import socketIO from "socket.io-client"
+import Nav from "./components/Nav"
 
+const socket = socketIO.connect("http://localhost:4000")
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Nav header="Bid Items" socket={socket}/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/products" element={<Products/>}/>
+          <Route path="/products/add" element={<AddProduct socket={socket}/>}/>
+          <Route path="/products/bid/:name/:price" element={<BidProduct socket={socket}/>}/>
+        </Routes>
+      </div>
   );
 }
 
